d56ff5eb84d906d46ad254e3387def0988d50c81
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-03-18 21:50:01 +0900

    feat(worker): とりあえず形だけ

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -5,20 +5,14 @@ import {
   generateGoParam,
 } from '@saitamau-maximum/auth'
 
-import handleProxy from './proxy'
-import handleRedirect from './redirect'
-import apiRouter from './router'
-
 // const authDomain = 'https://auth.maximum.vc'
 const authDomain = 'http://127.0.0.1:8788'
 
-// Export a default object containing event handlers
 export default {
-  // The fetch handler is invoked when this worker receives a HTTP(S) request
-  // and should return a Response (optionally wrapped in a Promise)
   async fetch(
     request: Request,
     env: Env,
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
     ctx: ExecutionContext,
   ): Promise<Response> {
     const url = new URL(request.url)
@@ -26,6 +20,12 @@ export default {
     const authName = 'maximum-reverse-proxy'
     const privateKey = await importKey(env.PRIVKEY, 'privateKey')
     const publicKey = await derivePublicKey(privateKey)
+
+    // Callback
+    if (url.pathname === '/auth/callback') {
+      // TODO: ログイン処理
+      throw new Error('Not implemented')
+    }
 
     // ログインしてない場合はログインページに移動
     const loggedIn = false
@@ -61,27 +61,15 @@ export default {
       })
     }
 
-    // You can get pretty far with simple logic like if/switch-statements
-    switch (url.pathname) {
-      case '/redirect':
-        return handleRedirect.fetch(request, env, ctx)
+    // それ以外の場合は Proxy
+    const res = await fetch(url.toString(), {
+      ...request,
+      headers: {
+        ...request.headers,
+        // TODO: 専用のヘッダーを追加
+      },
+    })
 
-      case '/proxy':
-        return handleProxy.fetch(request, env, ctx)
-    }
-
-    if (url.pathname.startsWith('/api/')) {
-      // You can also use more robust routing
-      return apiRouter.handle(request)
-    }
-
-    return new Response(
-      `Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-      { headers: { 'Content-Type': 'text/html' } },
-    )
+    return res
   },
 }
