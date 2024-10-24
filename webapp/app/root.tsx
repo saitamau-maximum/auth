66a577d69704351aa2d574483c948a3aec753618
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-10-24 14:45:17 +0900

    fix stylesheet duplicates

--- webapp/app/root.tsx
+++ webapp/app/root.tsx
@@ -45,16 +45,6 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap'
-          rel='stylesheet'
-        />
         <Meta />
         <Links />
       </head>
@@ -76,6 +66,6 @@ export const links: LinksFunction = () => [
   },
   {
     rel: 'stylesheet',
-    href: `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap&text=${usedCharacters}`,
+    href: `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap`,
   },
 ]
