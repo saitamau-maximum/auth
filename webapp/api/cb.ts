eda7e98a6aa6a3f6a72e08a0da3b3b6663cd5a44
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-10-28 16:08:34 +0900

    impl `/cb`

---
+++ webapp/api/cb.ts
@@ -0,0 +1,132 @@
+import { createAppAuth } from '@octokit/auth-app'
+import { Hono } from 'hono'
+import { validator } from 'hono/validator'
+import { Octokit } from 'octokit'
+import cookieSessionStorage from 'utils/session.server'
+
+import { Env } from '../load-context'
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.get(
+  '/',
+  validator('query', (value, c) => {
+    const { code, state } = value
+
+    if (typeof code !== 'string' || typeof state !== 'string') {
+      return c.text('invalid request', 400)
+    }
+
+    return { code, state }
+  }),
+  async c => {
+    const { code, state } = c.req.valid('query')
+
+    const { getSession, commitSession } = cookieSessionStorage(c.env)
+    const session = await getSession(c.req.header('Cookie'))
+
+    if (state !== session.get('state')) {
+      c.header('Set-Cookie', await commitSession(session))
+      return c.text('state mismatch', 400)
+    }
+
+    interface GitHubOAuthTokenResponse {
+      access_token: string
+      scope: string
+      token_type: string
+    }
+
+    const { access_token } = await fetch(
+      'https://github.com/login/oauth/access_token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({
+          client_id: c.env.GITHUB_OAUTH_ID,
+          client_secret: c.env.GITHUB_OAUTH_SECRET,
+          code,
+        }),
+      },
+    ).then(res => res.json<GitHubOAuthTokenResponse>())
+
+    // ----- メンバーの所属判定 ----- //
+    const userOctokit = new Octokit({ auth: access_token })
+    const appOctokit = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: c.env.GITHUB_APP_ID,
+        privateKey: atob(c.env.GITHUB_APP_PRIVKEY),
+        installationId: '41674415',
+      },
+    })
+
+    const { data: user } = await userOctokit.request('GET /user')
+
+    let isMember = false
+    try {
+      const checkIsOrgMemberRes = await appOctokit.request(
+        'GET /orgs/{org}/members/{username}',
+        {
+          org: 'saitamau-maximum',
+          username: user.login,
+        },
+      )
+      isMember = (checkIsOrgMemberRes.status as number) === 204
+    } catch (_) {
+      isMember = false
+    }
+
+    session.set('id', String(user.id))
+    session.set('display_name', user.login)
+    session.set('profile_image', user.avatar_url)
+
+    if (!isMember) {
+      session.set('is_member', false)
+      session.set('teams', [])
+    } else {
+      session.set('is_member', true)
+
+      // チーム数およびメンバー数が 100 以下である前提
+      // 超える場合には Pagination を用いて取得する必要がある
+      const { data: teams } = await appOctokit.request(
+        'GET /orgs/{org}/teams',
+        {
+          org: 'saitamau-maximum',
+          per_page: 100,
+        },
+      )
+      const teamsMembers = await Promise.all(
+        teams.map(async team => {
+          const { data: members } = await appOctokit.request(
+            'GET /orgs/{org}/teams/{team_slug}/members',
+            {
+              org: 'saitamau-maximum',
+              team_slug: team.slug,
+              per_page: 100,
+            },
+          )
+          return [team.name, members] as const
+        }),
+      )
+
+      session.set(
+        'teams',
+        teamsMembers
+          .filter(team => team[1].some(member => member.id === user.id))
+          .map(team => team[0]),
+      )
+    }
+
+    c.header('Set-Cookie', await commitSession(session))
+    return c.redirect('/continue', 302)
+  },
+)
+
+app.all('/', async c => {
+  return c.text('method not allowed', 405)
+})
+
+export default app
--- webapp/api/index.ts
+++ webapp/api/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 
 import { Env } from '../load-context'
 
+import cbRoute from './cb'
 import goRoute from './go'
 import tokenRoute from './token'
 
@@ -10,7 +11,9 @@ const app = new Hono<{ Bindings: Env }>()
 app.use(async (c, next) => {
   await next()
 })
+
 app.route('/token', tokenRoute)
 app.route('/go', goRoute)
+app.route('/cb', cbRoute)
 
 export default app
