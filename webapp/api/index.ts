4f036c08ccd1c7e2eff6d5bc617b0799c199303d
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-10-28 18:02:22 +0900

    `secureHeaders` 追加

--- webapp/api/index.ts
+++ webapp/api/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { secureHeaders } from 'hono/secure-headers'
 
 import { Env } from '../load-context'
 
@@ -7,6 +8,8 @@ import goRoute from './go'
 import tokenRoute from './token'
 
 const app = new Hono<{ Bindings: Env }>()
+
+app.use(secureHeaders())
 
 app.use(async (c, next) => {
   await next()
