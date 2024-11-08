2d423eff96a6d767e5899be85596e5347c25cfe2
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-08 18:12:32 +0900

    `/oauth/authorize` を relation を用いて refactor

--- webapp/api/oauth/authorize.ts
+++ webapp/api/oauth/authorize.ts
@@ -1,5 +1,4 @@
 import { importKey } from '@saitamau-maximum/auth/internal'
-import { and, eq, inArray } from 'drizzle-orm'
 import { Context, Hono } from 'hono'
 import { html } from 'hono/html'
 import { validator } from 'hono/validator'
@@ -7,33 +6,13 @@ import { HonoEnv } from 'load-context'
 import { generateAuthToken } from 'utils/auth-token.server'
 import { z } from 'zod'
 
-import {
-  oauthClient,
-  oauthClientScope,
-  oauthScope,
-  user,
-} from '../../app/schema'
-
 const app = new Hono<HonoEnv>()
-
-interface IDbScope {
-  oauth_scope: {
-    id: number
-    name: string
-    description: string | null
-  } | null
-  oauth_client_scope: {
-    client_id: string
-    scope_id: number
-  }
-}
 
 // 仕様はここ参照: https://github.com/saitamau-maximum/auth/issues/27
 
 app.get(
   '/',
   // TODO: Bad Request の画面をいい感じにするかも
-  // zValidator 使うと DB 二重でリクエストしそう 使い方が悪い？
   validator('query', async (query, c: Context<HonoEnv>) => {
     // client_id がパラメータにあるか・複数存在しないか
     const { data: clientId, success: success1 } = z
@@ -44,15 +23,18 @@ app.get(
     }
 
     // client_id が DB にあるか
-    const client = (
-      await c.var.dbClient
-        .select()
-        .from(oauthClient)
-        .leftJoin(user, eq(oauthClient.owner_id, user.id))
-        .where(eq(oauthClient.id, clientId))
-        .limit(1)
-    ).at(0)
-    // LIMIT 1 なので client.length は 0 or 1 で、条件に合致するものがなかったら [0] が undefined になる
+    const client = await c.var.dbClient.query.oauthClient.findFirst({
+      where: (oauthClient, { eq }) => eq(oauthClient.id, clientId),
+      with: {
+        callbacks: true,
+        owner: true,
+        scopes: {
+          with: {
+            scope: true,
+          },
+        },
+      },
+    })
     if (!client) return c.text('Bad Request: client_id not registered', 400)
 
     // redirect_uri が複数ないことをチェック
@@ -68,34 +50,23 @@ app.get(
 
     // redirect_uri がパラメータとして与えられていない場合
     if (!redirectUri) {
-      const registeredUris =
-        await c.var.dbClient.query.oauthClientCallback.findMany({
-          where: (oauthClientCallback, { eq }) =>
-            eq(oauthClientCallback.client_id, clientId),
-        })
-
-      if (registeredUris.length === 0) {
+      if (client.callbacks.length === 0) {
         return c.text('Bad Request: redirect_uri not registered', 400)
       }
-      if (registeredUris.length > 1) {
+      if (client.callbacks.length > 1) {
         return c.text('Bad Request: ambiguous redirect_uri', 400)
       }
 
       // DB 内に登録されているものを callback として扱う
-      redirectUri = registeredUris[0].callback_url
+      redirectUri = client.callbacks[0].callback_url
     } else {
       // Redirect URI のクエリパラメータ部分は変わることを許容する
       const normalizedUri = new URL(redirectUri)
       normalizedUri.search = ''
 
-      const registeredUri =
-        await c.var.dbClient.query.oauthClientCallback.findFirst({
-          where: (oauthClientCallback, { eq, and }) =>
-            and(
-              eq(oauthClientCallback.client_id, clientId),
-              eq(oauthClientCallback.callback_url, normalizedUri.toString()),
-            ),
-        })
+      const registeredUri = client.callbacks.find(
+        data => data.callback_url === normalizedUri.toString(),
+      )
 
       if (!registeredUri) {
         return c.text('Bad Request: redirect_uri not registered', 400)
@@ -152,18 +123,10 @@ app.get(
       return errorRedirect('invalid_scope', 'invalid scope', '')
     }
 
-    let dbScopes: IDbScope[]
-
-    if (!scope) {
-      dbScopes = await c.var.dbClient
-        .select()
-        .from(oauthClientScope)
-        .leftJoin(oauthScope, eq(oauthClientScope.scope_id, oauthScope.id))
-        .where(eq(oauthClientScope.client_id, clientId))
-    } else {
+    if (scope) {
       const scopes = scope.split(' ')
-      const uniqueScopes = [...new Set(scope.split(' '))]
-      if (scopes.length !== uniqueScopes.length) {
+      const scopeSet = new Set(scope.split(' '))
+      if (scopes.length !== scopeSet.size) {
         return errorRedirect(
           'invalid_scope',
           'there are duplicates in scopes',
@@ -171,19 +134,8 @@ app.get(
         )
       }
 
-      dbScopes = await c.var.dbClient
-        .select()
-        .from(oauthClientScope)
-        .leftJoin(oauthScope, eq(oauthClientScope.scope_id, oauthScope.id))
-        .where(
-          and(
-            eq(oauthClientScope.client_id, clientId),
-            inArray(oauthScope.name, scopes),
-          ),
-        )
-      const dbScopesSet = new Set(
-        dbScopes.map(scope => scope.oauth_scope?.name),
-      )
+      const dbScopesSet = new Set(client.scopes.map(scope => scope.scope.name))
+
       const unknownScopes = scopes.filter(scope => !dbScopesSet.has(scope))
       if (unknownScopes.length > 0) {
         return errorRedirect(
@@ -192,9 +144,13 @@ app.get(
           '',
         )
       }
+
+      client.scopes = client.scopes.filter(scope =>
+        scopeSet.has(scope.scope.name),
+      )
     }
 
-    if (dbScopes.length === 0) {
+    if (client.scopes.length === 0) {
       return errorRedirect(
         'invalid_scope',
         'there must be at least one scope specified',
@@ -207,7 +163,6 @@ app.get(
       redirectUri,
       state: state || '',
       scope: scope || '',
-      dbScopes,
       clientInfo: client,
     }
   }),
@@ -216,7 +171,7 @@ app.get(
     return next()
   },
   async c => {
-    const { clientId, redirectUri, state, scope, dbScopes, clientInfo } =
+    const { clientId, redirectUri, state, scope, clientInfo } =
       c.req.valid('query')
     const nowUnixMs = Date.now()
 
@@ -235,23 +190,19 @@ app.get(
     const responseHtml = html`<!doctype html>
       <html lang="ja">
         <head>
-          <title>
-            Authorize ${clientInfo.oauth_client.name} | Maximum Auth
-          </title>
+          <title>Authorize ${clientInfo.name} | Maximum Auth</title>
         </head>
         <body>
-          <h1>${clientInfo.oauth_client.name} を承認しますか？</h1>
+          <h1>${clientInfo.name} を承認しますか？</h1>
           <div>
-            承認すると、 ${clientInfo.user?.displayName} による
-            ${clientInfo.oauth_client.name} はあなたのアカウント
+            承認すると、 ${clientInfo.owner.displayName} による
+            ${clientInfo.name} はあなたのアカウント
             (ここにログインユーザーの情報を入れる)
             の以下の情報にアクセスできるようになります。
             <ul>
-              ${dbScopes.map(
+              ${clientInfo.scopes.map(
                 data =>
-                  html`<li>
-                    ${data.oauth_scope?.name}: ${data.oauth_scope?.description}
-                  </li>`,
+                  html`<li>${data.scope.name}: ${data.scope.description}</li>`,
               )}
             </ul>
           </div>
