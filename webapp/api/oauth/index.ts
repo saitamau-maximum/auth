272c6664a8f7df5bdb49af692194310328ead575
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-11-13 17:55:36 +0900

    `/access-token` 実装

--- webapp/api/oauth/index.ts
+++ webapp/api/oauth/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 
 import { HonoEnv } from '../../load-context'
 
+import oauthAccesstokenRoute from './accessToken'
 import oauthAuthorizeRoute from './authorize'
 import oauthCallbackRoute from './callback'
 
@@ -9,5 +10,6 @@ const app = new Hono<HonoEnv>()
 
 app.route('/authorize', oauthAuthorizeRoute)
 app.route('/callback', oauthCallbackRoute)
+app.route('/access-token', oauthAccesstokenRoute)
 
 export default app
