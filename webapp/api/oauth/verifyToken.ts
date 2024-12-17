3bbec15aa61ce0e55a80f54fe14811b318ff94e7
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-17 19:56:52 +0900

    fix: use header for getting access token in verify-token (#51)

--- webapp/api/oauth/verifyToken.ts
+++ webapp/api/oauth/verifyToken.ts
@@ -1,10 +1,10 @@
-import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
-import { z } from 'zod'
 
 import { READ_SCOPES } from '../../constants/scope'
 import { HonoEnv } from '../../load-context'
 import { IUserInfo } from '../../repository/idp'
+
+import { authMiddleware } from './_middleware'
 
 const app = new Hono<HonoEnv>()
 
@@ -41,64 +41,32 @@ const INVALID_REQUEST_RESPONSE: InvalidResponseType = {
   scopes: null,
 }
 
-app.post(
-  '/',
-  zValidator(
-    'form',
-    z.object({
-      access_token: z.string(),
-    }),
-    async (res, c) => {
-      if (!res.success)
-        return c.json<InvalidResponseType>(INVALID_REQUEST_RESPONSE, 400)
-    },
-  ),
-  async c => {
-    const { access_token } = c.req.valid('form')
+app.get('/', authMiddleware, async c => {
+  const tokenInfo = c.var.tokenInfo
 
-    const nowUnixMs = Date.now()
-    const nowDate = new Date(nowUnixMs)
+  c.header('Cache-Control', 'no-store')
+  c.header('Pragma', 'no-cache')
 
-    const tokenInfo = await c.var.dbClient.query.token.findFirst({
-      where: (token, { eq, and, gt }) =>
-        and(
-          eq(token.access_token, access_token),
-          gt(token.code_expires_at, nowDate),
-        ),
-      with: {
-        client: true,
-        scopes: {
-          with: {
-            scope: true,
-          },
-        },
-      },
-    })
+  // Token が見つからない場合
+  if (!tokenInfo) {
+    return c.json<InvalidResponseType>(INVALID_REQUEST_RESPONSE, 404)
+  }
 
-    c.header('Cache-Control', 'no-store')
-    c.header('Pragma', 'no-cache')
+  const res: ValidResponseType = {
+    valid: true,
+    client: tokenInfo.client,
+    user_id: tokenInfo.user_id,
+    expires_at: tokenInfo.access_token_expires_at.getTime(),
+    scopes: tokenInfo.scopes.map(s => s.scope.name),
+  }
 
-    // Token が見つからない場合
-    if (!tokenInfo) {
-      return c.json<InvalidResponseType>(INVALID_REQUEST_RESPONSE, 404)
-    }
+  if (res.scopes.includes(READ_SCOPES.BASIC_INFO)) {
+    const user = await c.var.idpClient.findUserById(res.user_id)
+    if (user) res.user_info = user
+  }
 
-    const res: ValidResponseType = {
-      valid: true,
-      client: tokenInfo.client,
-      user_id: tokenInfo.user_id,
-      expires_at: tokenInfo.access_token_expires_at.getTime(),
-      scopes: tokenInfo.scopes.map(s => s.scope.name),
-    }
-
-    if (res.scopes.includes(READ_SCOPES.BASIC_INFO)) {
-      const user = await c.var.idpClient.findUserById(res.user_id)
-      if (user) res.user_info = user
-    }
-
-    return c.json<ValidResponseType>(res)
-  },
-)
+  return c.json<ValidResponseType>(res)
+})
 
 // POST 以外は許容しない
 app.all('/', async c => {
